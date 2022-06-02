274db4ad08ea46f9160cdc9e6347fdc273134b2b
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-06-02 13:06:26 +0200

    Add a calculate Button and use selctor using Ojactive-C feature

--- Window shopper/Controller/MainViewController.swift
+++ Window shopper/Controller/MainViewController.swift
@@ -2,11 +2,24 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var wegeText: CurrencyTextField!
+    @IBOutlet weak var priceText: CurrencyTextField!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let calculateButton = UIButton(
+            frame: CGRect(x: 0 , y: 0, width: view.frame.size.width, height:60))
+        calculateButton.backgroundColor = UIColor.orange
+        calculateButton.setTitle("Calculate", for: .normal)
+        calculateButton.setTitle("\(UIColor.white)", for: .normal)
+        calculateButton.addTarget(self, action:
+                                    #selector(MainViewController.calculate), for: .touchUpInside)
+        
     }
-
-
+    
+    @objc func calculate() {
+        
+    }
 }
 
