8c94a9ea09df5c58a85111a3562aeda7dc30ab6c
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-06-11 17:42:01 +0200

    Connent the logic with UI

--- Window shopper/Controller/MainViewController.swift
+++ Window shopper/Controller/MainViewController.swift
@@ -2,10 +2,19 @@ import UIKit
 
 class MainViewController: UIViewController {
 
-    @IBOutlet weak var wegeText: CurrencyTextField!
+    @IBOutlet weak var wageText: CurrencyTextField!
     @IBOutlet weak var priceText: CurrencyTextField!
     
-
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var hoursLabel: UILabel!
+    
+    @IBAction func clearCalculator(_ sender: Any) {
+        resultLabel.isHidden = true
+        hoursLabel.isHidden = true
+        wageText.text = " "
+        priceText.text = " "
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let calculateButton = UIButton(
@@ -15,13 +24,24 @@ class MainViewController: UIViewController {
         calculateButton.setTitleColor(UIColor.white, for: .normal)
         calculateButton.addTarget(self, action:
                                     #selector(MainViewController.calculate), for: .touchUpInside)
-        wegeText.inputAccessoryView = calculateButton
+        wageText.inputAccessoryView = calculateButton
         priceText.inputAccessoryView = calculateButton
+        
+        resultLabel.isHidden = true
+        hoursLabel.isHidden = true
         
     }
     
     @objc func calculate() {
-        print("here we go")
+        if let wageText = wageText.text, let priceText = priceText.text {
+            if let wage = Double(wageText), let price = Double(priceText) {
+                view.endEditing(true) // dismissing the keyboard
+                resultLabel.isHidden = false
+                hoursLabel.isHidden = false
+                resultLabel.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+            }
+        }
     }
+    
 }
 
