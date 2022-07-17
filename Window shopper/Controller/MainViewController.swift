54c107e52d5306e9ac419245f43c454d95a1a082
amjadoudeh <amjad.oudeh@soundcloud.com>
2022-07-17 19:45:54 +0200

    Fix the pleaceholder issue

--- Window shopper/Controller/MainViewController.swift
+++ Window shopper/Controller/MainViewController.swift
@@ -4,21 +4,21 @@ class MainViewController: UIViewController {
 
     @IBOutlet weak var wageText: CurrencyTextField!
     @IBOutlet weak var priceText: CurrencyTextField!
-    
+
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var hoursLabel: UILabel!
-    
+
     @IBAction func clearCalculator(_ sender: Any) {
         resultLabel.isHidden = true
         hoursLabel.isHidden = true
         wageText.text = ""
         priceText.text = ""
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         let calculateButton = UIButton(
-            frame: CGRect(x: 0 , y: 0, width: view.frame.size.width, height:60))
+            frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
         calculateButton.backgroundColor = UIColor.orange
         calculateButton.setTitle("Calculate", for: .normal)
         calculateButton.setTitleColor(UIColor.white, for: .normal)
@@ -26,12 +26,12 @@ class MainViewController: UIViewController {
                                     #selector(MainViewController.calculate), for: .touchUpInside)
         wageText.inputAccessoryView = calculateButton
         priceText.inputAccessoryView = calculateButton
-        
+
         resultLabel.isHidden = true
         hoursLabel.isHidden = true
-        
+
     }
-    
+
     @objc func calculate() {
         if let wageText = wageText.text, let priceText = priceText.text {
             if let wage = Double(wageText), let price = Double(priceText) {
@@ -42,6 +42,5 @@ class MainViewController: UIViewController {
             }
         }
     }
-    
-}
 
+}
--- Window shopper/Views /CurrencyTextField.swift
+++ Window shopper/Views /CurrencyTextField.swift
@@ -3,39 +3,35 @@ import UIKit
 @IBDesignable
 
 class CurrencyTextField: UITextField {
-    
-//    override func draw(_ rect: CGRect) {
-//
-//    }
-    
-    override  func prepareForInterfaceBuilder() {
+
+    override func prepareForInterfaceBuilder() {
         customizeView()
     }
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
         customizeView()
     }
-    
+
     func customizeView() {
         backgroundColor =  UIColor(white: 1, alpha: 0.3)
         layer.cornerRadius = 10.0
         textAlignment = .center
         textColor = UIColor.white
-        
+
         clipsToBounds = true
-        
+
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
             attributedPlaceholder = place
         }
-        
+
         let size: CGFloat = 20
         let currencyLabel = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
         currencyLabel.backgroundColor = UIColor.gray
         currencyLabel.textAlignment = .center
         currencyLabel.layer.cornerRadius = 5.0
-        //currencyLabel.text = UIColor(white: 1, alpha: 0.3)
+        // currencyLabel.text = UIColor(white: 1, alpha: 0.3)
         currencyLabel.clipsToBounds = true
         let formatter = NumberFormatter()
         formatter.numberStyle = .currency
@@ -43,5 +39,5 @@ class CurrencyTextField: UITextField {
         currencyLabel.text = formatter.currencySymbol
         addSubview(currencyLabel)
     }
-    
+
 }
